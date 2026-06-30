8dda9e7a7ba7e96e113e5d86c5518d53774e4ece
Yoelkys R Rodriguez Gonzalez <yoel.rrg@gmail.com>
2026-06-30 11:12:13 -0300

    vite chunk size

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Increase the limit to 1000 kB (1 MB)
+    chunkSizeWarningLimit: 1000, 
+  },
 })
